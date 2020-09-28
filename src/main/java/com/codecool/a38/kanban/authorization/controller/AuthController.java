d2dd92be161c5169fea1735433d8330c7f1f22e8
Tóth Máté <tothmate911@gmail.com>
2020-09-28 15:17:59 +0200

    save token in cookie

--- src/main/java/com/codecool/a38/kanban/authorization/controller/AuthController.java
+++ src/main/java/com/codecool/a38/kanban/authorization/controller/AuthController.java
@@ -1,15 +1,21 @@
 package com.codecool.a38.kanban.authorization.controller;
 
 import com.codecool.a38.kanban.authorization.model.OAuthResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@Slf4j
 public class AuthController {
 
     private static final String APP_ID = "458f27c6eb357cf7419231331e3af3e3a9d39782b7edf50ac2cc083e7a7f1a4a";
@@ -18,12 +24,14 @@ public class AuthController {
 
     private RestTemplate restTemplate;
 
+    private String token;
+
     public AuthController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping("/getToken")
-    public void getToken(@RequestParam String code) {
+    public ResponseEntity getToken(@RequestParam String code, HttpServletResponse response) {
         String url = "https://gitlab.techpm.guru/oauth/token";
         String parameters = "?client_id=" + APP_ID +
                 "&client_secret=" + APP_SECRET +
@@ -35,9 +43,18 @@ public class AuthController {
         OAuthResponse oAuthResponse = restTemplate.postForEntity(url + parameters,
                 request, OAuthResponse.class).getBody();
 
-        if (oAuthResponse != null) {
-            System.out.println("access token: " + oAuthResponse.getAccess_token());
-        }
+        log.info("access token: " + oAuthResponse.getAccess_token());
+        token = oAuthResponse.getAccess_token();
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setMaxAge(60 * 60 * 24);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
+
+        log.info("Token set: token: " + token);
+        return ResponseEntity.ok("token sent back");
     }
 
 }
