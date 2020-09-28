cf3963ea2263b9dad46d322714aa9dcea34a400f
Tóth Máté <tothmate911@gmail.com>
2020-09-28 13:36:54 +0200

    create OAuthResponse class

--- src/main/java/com/codecool/a38/kanban/authorization/controller/AuthController.java
+++ src/main/java/com/codecool/a38/kanban/authorization/controller/AuthController.java
@@ -1,25 +1,43 @@
 package com.codecool.a38.kanban.authorization.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
 
-    private static final String applicationId = "458f27c6eb357cf7419231331e3af3e3a9d39782b7edf50ac2cc083e7a7f1a4a";
-    private static final String secret = "f0fbf238c1ef5d0be56bf1118c430b15daff2b85d790d4bbfd76b8ccbb5bac33";
-    private static final String redir = "http://localhost:8080/getToken";
+    private static final String APP_ID = "458f27c6eb357cf7419231331e3af3e3a9d39782b7edf50ac2cc083e7a7f1a4a";
+    private static final String APP_SECRET = "f0fbf238c1ef5d0be56bf1118c430b15daff2b85d790d4bbfd76b8ccbb5bac33";
+    private static final String REDIRECT_URI = "http://localhost:8080/getToken";
 
+    private RestTemplate restTemplate;
+
+    public AuthController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/getToken")
     public void getToken(@RequestParam String code) {
         System.out.println("get Token: " + code);
 
-        String grant_type = "authorization_code";
+        String url = "https://gitlab.techpm.guru/oauth/token";
+        String parameters = "?client_id=" + APP_ID +
+                "&client_secret=" + APP_SECRET +
+                "&code=" + code +
+                "&grant_type=authorization_code" +
+                "&redirect_uri=" + REDIRECT_URI;
 
+        HttpEntity<String> request = new HttpEntity<>(null);
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url + parameters, request, String.class);
+
+        System.out.println(responseEntity.getBody());
 
 
     }
