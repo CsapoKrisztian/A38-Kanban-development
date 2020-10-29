86ca576bb2f6342242415a3ca033dc5ce015be6f
Tóth Máté <tothmate911@gmail.com>
2020-10-29 11:38:43 +0100

    a

--- src/main/java/com/codecool/a38/kanban/config/service/ConfigDataProvider.java
+++ src/main/java/com/codecool/a38/kanban/config/service/ConfigDataProvider.java
@@ -34,22 +34,25 @@ public class ConfigDataProvider {
     @PostConstruct
     public void init() {
         ObjectMapper mapper = new ObjectMapper();
-//        TypeReference<JsonProperties> typeReference = new TypeReference<>() {
-//        };
-//        InputStream inputStream = TypeReference.class.getResourceAsStream("/configprops.json");
-//        try {
-//            JsonProperties jsonProperties = mapper.readValue(inputStream, typeReference);
-//            setDataFromJsonProperties(jsonProperties);
-//            log.info("Config properties loaded from configprops.json");
-//            log.info("Status titles: " + statusTitles.toString());
-//            log.info("Story prefix: " + storyPrefix);
-//            log.info("statusTitleDisplayMap: " + statusTitleDisplayMap.toString());
-//            log.info("priorityTitleDisplayMap: " + priorityTitleDisplayMap.toString());
-//
-//        } catch (IOException e) {
-//            log.info("Unable to read configprops.json");
-//            e.printStackTrace();
-//        }
+        TypeReference<JsonProperties> typeReference = new TypeReference<>() {
+        };
+        InputStream inputStream = TypeReference.class.getResourceAsStream("/configprops.json");
+        try {
+            JsonProperties jsonProperties = mapper.readValue(inputStream, typeReference);
+            setDataFromJsonProperties(jsonProperties);
+            log.info("Config properties loaded from configprops.json");
+            log.info("Status titles: " + statusTitles.toString());
+            log.info("Story prefix: " + storyPrefix);
+            log.info("statusTitleDisplayMap: " + statusTitleDisplayMap.toString());
+            log.info("priorityTitleDisplayMap: " + priorityTitleDisplayMap.toString());
+
+        } catch (IOException e) {
+            log.info("Unable to read configprops.json");
+            e.printStackTrace();
+        } catch (Exception e) {
+            log.info("!!!!!");
+            e.printStackTrace();
+        }
     }
 
     private void setDataFromJsonProperties(JsonProperties jsonProperties) {
